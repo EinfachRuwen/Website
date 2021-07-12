5cb568e22570011aaf6cc0cf0027f626d85696af
byZeroOfficial <76685126+byZeroOfficial@users.noreply.github.com>
2021-07-12 17:53:11 +0200

    Update footer.js

--- src/pages/utils/footer.js
+++ src/pages/utils/footer.js
@@ -8,7 +8,7 @@ class Footer extends React.Component {
                 {JSON.stringify(config.customization.socials) !== "{}" ? <div className="text-left md:ml-10 ml-5 text-gray-500">Follow me on</div> : <></>}
                 <div className="flex items-center md:px-10 px-3">
                     <img alt="" src="./image/icon_discord.png" className="inline-block rounded-lg w-10 h-10 cursor-pointer mr-2" id="icon_twitter" onClick={() => window.open('https://discord.com/users/390965278470569985')} />
-                    <img alt="" src="https://keybase.io/images/icons/icon-keybase-logo-48.png" className="inline-block rounded-lg w-10 h-10 cursor-pointer mr-2" id="icon_keybase" onClick={() => window.open('https://keybase.io/byzero')} />
+                    <img alt="" src="https://element.io/images/logo-mark-primary.svg" className="inline-block rounded-lg w-10 h-10 cursor-pointer mr-2" id="icon_matrix" onClick={() => window.open('https://byzero.ml/matrix')} />
                     <img alt="" src="./image/icon_github.png" className="inline-block rounded-lg w-10 h-10 cursor-pointer mr-2" id="icon_patreon" onClick={() => window.open('https://byzer0.ml/github')} /> 
                     <img alt="" src="./image/icon_twitch.png" className="inline-block rounded-lg w-10 h-10 cursor-pointer mr-2" id="icon_youtube" onClick={() => window.open('https://byzer0.ml/twitch')} /> 
                     <img alt="" src="./image/icon_mail.png" className="inline-block rounded-lg w-10 h-10 cursor-pointer mr-2" id="icon_reddit" onClick={() => window.open('mailto:business@byzero.ml')} />
@@ -16,6 +16,7 @@ class Footer extends React.Component {
                     <img alt="" src="./image/icon_patreon.png" className="inline-block rounded-lg w-10 h-10 cursor-pointer mr-2" id="icon_patreon" onClick={() => window.open('https://byzer0.ml/patreon')} /> 
                     <img alt="" src="./image/icon_youtube.png" className="inline-block rounded-lg w-10 h-10 cursor-pointer mr-2" id="icon_youtube" onClick={() => window.open('https://byzer0.ml/yt')} /> 
                     <img alt="" src="./image/icon_reddit.png" className="inline-block rounded-lg w-10 h-10 cursor-pointer mr-2" id="icon_reddit" onClick={() => window.open('https://byzer0.ml/reddit')} /> 
+                    <img alt="" src="https://protonmail.com/blog/wp-content/themes/bootstrap-basic/img/stripeLogo.png" className="inline-block rounded-lg w-10 h-10 cursor-pointer mr-2" id="icon_protonmail" onClick={() => window.open('mailto:mail.byzero@protonmail.com')} /> 
                     <img alt="" src="./image/icon_donate.png" className="inline-block rounded-lg w-10 h-10 cursor-pointer mr-2" id="icon_donate" onClick={() => window.open('https://byzer0.ml/donate')} />
                 </div>
                 &copy; 2021 <a href="https://byzero.ml" className="cursor-pointer transition duration-200 hover:text-blue-500 text-blue-300">byZero</a> | Made with <a href="https://byzer0.ml/generator" className="cursor-pointer transition duration-200 hover:text-blue-500 text-blue-300">byZero's Website Generator</a>

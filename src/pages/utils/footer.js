bd3746f2cb43027dcdc9d87cd7c03d536e456aea
byZeroOfficial <76685126+byZeroOfficial@users.noreply.github.com>
2021-09-18 11:21:59 +0200

    Update footer.js

--- src/pages/utils/footer.js
+++ src/pages/utils/footer.js
@@ -7,10 +7,8 @@ class Footer extends React.Component {
             <footer className="w-screen text-center py-3 bg-gray-900">
                 {JSON.stringify(config.customization.socials) !== "{}" ? <div className="text-left md:ml-10 ml-5 text-gray-500">Find me on</div> : <></>}
                 <div className="flex items-center md:px-10 px-3">
-                    <img alt="" src="./image/icon_discord.png" className="inline-block rounded-lg w-10 h-10 cursor-pointer mr-2" id="icon_twitter" onClick={() => window.open('https://discord.com/users/390965278470569985')} />
                     <img alt="" src="https://element.io/images/logo-mark-primary.svg" className="inline-block rounded-lg w-10 h-10 cursor-pointer mr-2" id="icon_matrix" onClick={() => window.open('https://byzer0.ml/matrix')} />
                     <img alt="" src="./image/icon_github.png" className="inline-block rounded-lg w-10 h-10 cursor-pointer mr-2" id="icon_patreon" onClick={() => window.open('https://byzer0.ml/github')} /> 
-                    <img alt="" src="./image/icon_twitch.png" className="inline-block rounded-lg w-10 h-10 cursor-pointer mr-2" id="icon_youtube" onClick={() => window.open('https://byzer0.ml/twitch')} /> 
                     <img alt="" src="./image/icon_mail.png" className="inline-block rounded-lg w-10 h-10 cursor-pointer mr-2" id="icon_reddit" onClick={() => window.open('mailto:business@byzero.ml')} />
                     <img alt="" src="./image/icon_twitter.png" className="inline-block rounded-lg w-10 h-10 cursor-pointer mr-2" id="icon_twitter" onClick={() => window.open('https://byzer0.ml/twitter')} /> 
                     <img alt="" src="./image/icon_patreon.png" className="inline-block rounded-lg w-10 h-10 cursor-pointer mr-2" id="icon_patreon" onClick={() => window.open('https://byzer0.ml/patreon')} /> 
